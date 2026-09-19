4f583af7cfac49b5b1cf22243b97375e7e998a13
Nidal <nidal0xp@users.noreply.github.com>
2026-09-19 02:00:28 +0100

    release: focus open source repository on Windows media center, Windows executables, and Windows source code

--- Run-Nidalplayer.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-if exist "%~dp0dist\Nidalplayer-Portable.exe" (
-    start "" "%~dp0dist\Nidalplayer-Portable.exe"
-) else (
-    start "" "%~dp0dist\win-unpacked\Nidalplayer.exe"
-)
--- scripts/build-android-tv.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
-
-const rootDir = path.join(__dirname, '..');
-const packScript = path.join(rootDir, 'scripts', 'pack_apk.py');
-
-console.log('Running APK build via pack_apk.py with strict UNIX path normalization...');
-execSync(`python "${packScript}"`, { stdio: 'inherit' });
